c15f7d9a14f7c89a36243ea6c782614d72d0fe3e
Crowdbotics <engineering@crowdbotics.com>
2023-11-09 16:20:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-docker-file-d-127986",
+  url: "https://test-docker-file-d-127986.botics.co",
+  api: "https://test-docker-file-d-127986.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
